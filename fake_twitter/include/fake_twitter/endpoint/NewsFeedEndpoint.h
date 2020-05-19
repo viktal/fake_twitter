7eedf6bf6fe9af22273e50376e3951cf2fd3872c
Viktoria Talmaza <vp.talmaza@gmail.com>
2020-05-20 02:02:32 +0300

    refactoring

--- fake_twitter/include/fake_twitter/endpoint/NewsFeedEndpoint.h
+++ fake_twitter/include/fake_twitter/endpoint/NewsFeedEndpoint.h
@@ -1,20 +1,21 @@
 #pragma once
+#include <fake_twitter/serializer/json.h>
 #include <pistache/router.h>
 #include <sqlpp11/sqlpp11.h>
 
-#include <fake_twitter/serializer/json.h>
 #include "fake_twitter/repository/NewsFeedRepository.h"
 
 namespace fake_twitter::endpoints {
-    class NewsFeedEndpoint {
-    public:
-        explicit NewsFeedEndpoint(std::shared_ptr<repository::NewsFeedRepository>
-        newsFeedRepository);
+class NewsFeedEndpoint {
+public:
+    explicit NewsFeedEndpoint(
+        std::shared_ptr<repository::NewsFeedRepository> newsFeedRepository);
 
-        void show(const Pistache::Http::Request &request, Pistache::Http::ResponseWriter response);
+    void show(const Pistache::Http::Request& request,
+              Pistache::Http::ResponseWriter response);
 
-    private:
-        std::shared_ptr<repository::NewsFeedRepository> newsFeedRepository;
-    };
+private:
+    std::shared_ptr<repository::NewsFeedRepository> newsFeedRepository;
+};
 
-}
+}  // namespace fake_twitter::endpoints
--- fake_twitter/src/endpoint/NewsFeedEndpoint.cpp
+++ fake_twitter/src/endpoint/NewsFeedEndpoint.cpp
@@ -3,7 +3,8 @@
 using namespace fake_twitter;
 using namespace fake_twitter::endpoints;
 
-void NewsFeedEndpoint::show(const Pistache::Http::Request &request, Pistache::Http::ResponseWriter response) {
+void NewsFeedEndpoint::show(const Pistache::Http::Request& request,
+                            Pistache::Http::ResponseWriter response) {
     using fake_twitter::sqlpp_models::TabTweets;
     auto id_optional = request.query().get("id");
     if (id_optional.isEmpty()) {
@@ -13,14 +14,16 @@ void NewsFeedEndpoint::show(const Pistache::Http::Request &request, Pistache::Ht
     auto id = std::stol(id_optional.get());
     std::vector<model::Tweet> tweetVector = newsFeedRepository->get(id);
     if (tweetVector.empty()) {
-        response.send(Pistache::Http::Code::Bad_Request, "No followers with this id");
+        response.send(Pistache::Http::Code::Bad_Request,
+                      "No followers with this id");
         return;
     }
-    response.headers().add<Pistache::Http::Header::ContentType>(MIME(Application, Json));
-    response.send(Pistache::Http::Code::Ok, serialization::to_json(tweetVector));
-
+    response.headers().add<Pistache::Http::Header::ContentType>(
+        MIME(Application, Json));
+    response.send(Pistache::Http::Code::Ok,
+                  serialization::to_json(tweetVector));
 }
 NewsFeedEndpoint::NewsFeedEndpoint(
-    std::shared_ptr<repository::NewsFeedRepository> newsFeedRepository)  {
+    std::shared_ptr<repository::NewsFeedRepository> newsFeedRepository) {
     this->newsFeedRepository = std::move(newsFeedRepository);
 };
