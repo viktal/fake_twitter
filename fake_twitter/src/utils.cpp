68fcc4e43d54a5823f555c33b2793776f30fae61
govyadkin <mgovyadkin@gmail.com>
2020-05-31 12:32:01 +0300

    clean code

--- fake_twitter/src/utils.cpp
+++ fake_twitter/src/utils.cpp
@@ -1,52 +1,17 @@
 #include "fake_twitter/utils.h"
 
 using namespace fake_twitter::utils;
-/* submodule cryptopp
-#include "base64.h"
-#include "filters.h"
-#include "sha.h"
 
-std::string SHA256HashString(std::string aString){
-    std::string digest;
-    CryptoPP::SHA256 hash;
-
-    CryptoPP::StringSource foo(aString, true,
-                               new CryptoPP::HashFilter(hash,
-                                                        new
-CryptoPP::Base64Encoder ( new CryptoPP::StringSink(digest))));
-
-    return digest;
-}
-*/
-
-// TODO:add MAC and IP address(or use uuid)
 std::string fake_twitter::utils::salt() {
     auto now = std::chrono::system_clock::now();
     return date::format("%FT%TZ", now);
 }
-/*#include <crypt.h>
+
 fake_twitter::PasswordHash fake_twitter::utils::make_password_hash(
     const std::string& salt, const std::string& password) {
-    // TODO: proper hashing. std::hash is not consistent between runs
-    //    return std::hash<std::string>{}(salt + password);
-    const char *p = password.c_str();
-    const char *s = salt.c_str();
-    std::string pas = crypt(p, s);
-    //std::cout << pas;
-    delete [] p;
-    delete [] s;
-    //auto tohash = salt + password;
-    //return std::accumulate(tohash.begin(), tohash.end(), 0);
-    return pas;
-}
-*/
-fake_twitter::PasswordHash fake_twitter::utils::make_password_hash(
-    const std::string& salt, const std::string& password) {
-    // TODO: proper hashing. std::hash is not consistent between runs
-    //    return std::hash<std::string>{}(salt + password);
     auto tohash = salt + password;
     long res = 0;
-    int p = 71;
-    for (int a : tohash) res = ((res + a) * p) % LONG_MAX;
+    int p = 31;
+    for (int a : tohash) res = ((res + a - 64) * p) % LONG_MAX;
     return res;
 }
